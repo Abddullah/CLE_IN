6ddc6b6bc2aa26a36a67fc41fe3c3e0d253d0cad
Abdullah Shah <abddullahshah@gmail.com>
2024-10-18 17:44:21 +0500

    subscription [ADD]

--- src/screens/Subscription/index.js
+++ src/screens/Subscription/index.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import CustomHeader from '../../components/Header';
 import { t } from 'i18next';
@@ -24,18 +24,35 @@ const Subscription = ({ navigation }) => {
                 isLeft={true}
                 leftPress={() => { navigation.goBack() }}
             />
-            <View style={styles.boxContainer}>
-                <SubscriptionIcon />
+
+            <ScrollView
+                style={{ width: '90%' }}
+                contentContainerStyle={styles.scrollBar}
+                showsVerticalScrollIndicator={false}
+            >
+                <SubscriptionIcon marginTop={10} />
                 <Text style={[Typography.text_heading, { color: colors.White_Primary_01, marginTop: 10 }]}>{t('UpgradeToPremium')}</Text>
                 <Text style={[Typography.text_subHeading_1, { color: colors.black, marginTop: 10, marginBottom: 10 }]}>{t('UnlimitedPostJobs')}</Text>
 
+
+                <View style={styles.subscription}>
+                    <View style={styles.subscriptionHeading}>
+                        <Text style={[Typography.text_subHeading_1, { color: colors.BothWhite, }]}>{t('clein')}</Text>
+                    </View>
+
+                    <View style={{ height: 109, justifyContent: 'center', alignItems: 'center', }}>
+                        <Text style={[Typography.text_heading, { color: colors.black, fontSize: 26 }]}>{'01'}</Text>
+                        <Text style={[Typography.text_heading, { color: colors.Neutral_01, }]}>{t('MonthsString')}</Text>
+                        <Text style={[Typography.text_heading, { color: colors.Neutral_01, }]}>{'$ 9.00'}</Text>
+                    </View>
+                </View>
 
                 <CTAButton1 title={t('Subscribe')} />
 
                 <Text style={[Typography.text_subHeading_1, { color: colors.black, marginTop: 10 }]}>{t('WhenwillIbebilled')}</Text>
                 <Text style={[Typography.text_paragraph, { color: colors.black, marginTop: 10 }]}>{t('YourItunesAccount')}</Text>
+            </ScrollView>
 
-            </View>
         </View>
     );
 };
@@ -57,6 +74,11 @@ const createStyles = (colors, theme) => {
             alignItems: 'center',
             marginTop: 20,
         },
+        scrollBar: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: 50
+        },
         box: {
             marginTop: 10,
             height: 44,
@@ -66,5 +88,23 @@ const createStyles = (colors, theme) => {
             alignItems: 'center',
             backgroundColor: colors.Primary_03
         },
+        subscription: {
+            height: 144,
+            width: 100,
+            marginBottom: 20,
+            borderRadius: 15,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            borderColor: colors.Neutral_01,
+            borderWidth: .5
+        },
+        subscriptionHeading: {
+            width: '100%',
+            height: 35,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 13,
+            backgroundColor: colors.White_Primary_01
+        }
     });
 };
